6bfd9fff70d17c7c987c11aebaf393e7104d4a70
Jamffy <l836828946@gmail.com>
2017-07-31 08:45:16 +0800

    lab2 test mem tools

--- inc/mmu.h
+++ inc/mmu.h
@@ -75,6 +75,9 @@
 // Address in page table or page directory entry
 #define PTE_ADDR(pte)	((physaddr_t) (pte) & ~0xFFF)
 
+// Address in page table or 4-MByte page directory entry
+#define PTE_ADDR_EX(pte)	((physaddr_t) (pte) & ~0x3FFFF)
+
 // Control Register flags
 #define CR0_PE		0x00000001	// Protection Enable
 #define CR0_MP		0x00000002	// Monitor coProcessor
