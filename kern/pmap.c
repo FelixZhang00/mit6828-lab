6efd9822f25957e1d17dc5fb5424e9702e2de8c4
Jamffy <l836828946@gmail.com>
2017-08-05 10:09:43 +0800

    lab4 exercise3

--- kern/pmap.c
+++ kern/pmap.c
@@ -283,7 +283,7 @@ mem_init_mp(void)
             continue;
 
         physaddr_t kstacktop_i = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
-        boot_map_region(kern_pgdir,kstacktop_i,
+        boot_map_region(kern_pgdir,kstacktop_i-KSTKSIZE,
                         KSTKSIZE,PADDR(percpu_kstacks[c-cpus]),PTE_W);
     }
 }
