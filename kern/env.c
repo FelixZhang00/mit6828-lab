f3426399f53e03efb5b2368671b1d122315fa9e1
FelixZhang00 <l836828946@gmail.com>
2017-08-06 19:12:31 +0800

    fix to go through grade-lab4

--- kern/env.c
+++ kern/env.c
@@ -273,8 +273,9 @@ env_alloc(struct Env **newenv_store, envid_t parent_id)
 	// commit the allocation
 	env_free_list = e->env_link;
 	*newenv_store = e;
-
-	// cprintf("[%08x] new env %08x\n", curenv ? curenv->env_id : 0, e->env_id);
+	
+	//fixme uncommet on lab5
+	cprintf("[%08x] new env %08x\n", curenv ? curenv->env_id : 0, e->env_id);
 	return 0;
 }
 
@@ -438,8 +439,9 @@ env_free(struct Env *e)
 	if (e == curenv)
 		lcr3(PADDR(kern_pgdir));
 
+	//fixme uncomment on lab5
 	// Note the environment's demise.
-	// cprintf("[%08x] free env %08x\n", curenv ? curenv->env_id : 0, e->env_id);
+	 cprintf("[%08x] free env %08x\n", curenv ? curenv->env_id : 0, e->env_id);
 
 	// Flush all mapped pages in the user portion of the address space
 	static_assert(UTOP % PTSIZE == 0);
