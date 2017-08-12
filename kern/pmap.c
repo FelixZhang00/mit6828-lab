c1e7bf6e291c50588d5104028c72da550fc01b33
Jamffy <l836828946@gmail.com>
2017-08-12 19:21:33 +0800

    lab5-exercise10

--- kern/pmap.c
+++ kern/pmap.c
@@ -203,6 +203,7 @@ mem_init(void)
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
 	boot_map_region(kern_pgdir,UENVS,PTSIZE,PADDR(envs),PTE_U);
+	memset(envs,0,NENV * sizeof(struct Env));
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
@@ -225,7 +226,7 @@ mem_init(void)
 	// we just set up the mapping anyway.
 	// Permissions: kernel RW, user NONE
 	// Your code goes here:
-	boot_map_region(kern_pgdir, KERNBASE, 0x10000000, 0, PTE_W | (PTE_PS & PSE_ENABLED));
+	boot_map_region(kern_pgdir, KERNBASE, (0xffffffff-KERNBASE+1), 0, PTE_W | (PTE_PS & PSE_ENABLED));
 
 	// Initialize the SMP-related parts of the memory map
 	mem_init_mp();
