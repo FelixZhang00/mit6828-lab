b040c20e8ae9e1eb4e9ad0fd9975f9b5550c5869
Jamffy <l836828946@gmail.com>
2017-08-12 19:02:24 +0800

    lab5-exercise10

--- kern/pmap.h
+++ kern/pmap.h
@@ -49,7 +49,6 @@ _kaddr(const char *file, int line, physaddr_t pa)
 enum {
 	// For page_alloc, zero the returned physical page.
 	ALLOC_ZERO = 1<<0,
-	ALLOC_EXPG = 1<<1,
 };
 
 void	mem_init(void);
