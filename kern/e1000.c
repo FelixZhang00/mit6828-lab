e06d566488e3bca522b47fe8d27bfbcf1024e8fd
Jamffy <l836828946@gmail.com>
2017-08-13 13:28:30 +0800

    lab6-exercise4

--- kern/e1000.c
+++ kern/e1000.c
@@ -5,6 +5,10 @@ int
 e1000_attach(struct pci_func *pcif)
 {
     pci_func_enable(pcif);
+
+    e1000 = (uint32_t *) mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+    cprintf("E1000 status: 0x%08x\n", e1000[E1000_STATUS]);
+
     return 0;
 }
 
--- kern/e1000.h
+++ kern/e1000.h
@@ -2,9 +2,16 @@
 #define JOS_KERN_E1000_H
 
 #include <kern/pci.h>
+#include <kern/pmap.h>
+#include <inc/string.h>
+#include <inc/error.h>
+
+volatile uint32_t *e1000;
 
 #define E1000_VENDORID 0x8086
 #define E1000_DEVICEID 0x100e
+
+#define E1000_STATUS 		(0x00008>>2)
 
 int e1000_attach(struct pci_func *pcif);
 
